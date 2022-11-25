61962bc376ac4f5cc7cab2d6040ad9e288ca230c
Gui-dev <adrianon10@gmail.com>
2022-11-25 19:40:36 -0300

    feat: add new routes

--- src/shared/app/routes/index.ts
+++ src/shared/app/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express'
 
+import { categoriesRoutes } from './../../../modules/categories/infra/http/routes/categories.routes'
+import { productsRoutes } from './../../../modules/products/infra/http/routes/products.routes'
+import { ordersRoutes } from './../../../modules/orders/infra/http/routes/orders.routes'
+
 const routes = Router()
 
-routes.get('/', (req, res) => {
-  res.send('Hello World')
-})
+routes.use(categoriesRoutes)
+routes.use(productsRoutes)
+routes.use(ordersRoutes)
 
 export { routes }
