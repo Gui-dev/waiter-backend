d336e01d62de5c161748e83e08f8bc1232b853af
Gui-dev <adrianon10@gmail.com>
2022-11-28 04:13:50 -0300

    feat: add helper function store

--- src/modules/products/infra/http/controllers/ProductController.ts
+++ src/modules/products/infra/http/controllers/ProductController.ts
@@ -1,11 +1,28 @@
 import { Request, Response } from 'express'
 
 import { ListProductsUseCase } from '../../../useCases/ListProductsUseCase'
+import { CreateProductsProps, CreateProductsUseCase } from '../../../useCases/CreateProductsUseCase'
+import { createProductValidation } from '../../../validations/createProductValidation'
 
 export class ProductController {
   public async index (request: Request, response: Response): Promise<Response> {
     const listProductsUseCase = new ListProductsUseCase()
     const products = await listProductsUseCase.execute()
+    return response.status(201).json(products)
+  }
+
+  public async store (request: Request, response: Response): Promise<Response> {
+    const data = request.body
+    const file = request.file
+    const imagePath = file && file.filename
+    const dataParse = {
+      ...data,
+      imagePath,
+      ingredients: JSON.parse(data.ingredients)
+    }
+    const result: CreateProductsProps = createProductValidation.parse(dataParse)
+    const createProduct = new CreateProductsUseCase()
+    const products = await createProduct.execute(result)
     return response.status(201).json(products)
   }
 }
--- src/modules/products/infra/http/routes/products.routes.ts
+++ src/modules/products/infra/http/routes/products.routes.ts
@@ -1,15 +1,14 @@
 import { Router } from 'express'
 
 import { ProductController } from './../controllers/ProductController'
+import { upload } from '../../../../../config/multerConfig'
 
 const productsRoutes = Router()
 const product = new ProductController()
 
 productsRoutes.get('/products', product.index)
 
-productsRoutes.post('/products', (req, res) => {
-  res.send('OK')
-})
+productsRoutes.post('/products', upload.single('image'), product.store)
 
 productsRoutes.get('/categories/:categoty_id/products', (req, res) => {
   res.send('OK')
