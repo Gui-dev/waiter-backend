a4a082d1b62ea266adb767b09288ee9c75f14cf2
Gui-dev <adrianon10@gmail.com>
2022-11-29 04:18:15 -0300

    feat: add helper method show

--- src/modules/categories/infra/http/controllers/CategoryController.ts
+++ src/modules/categories/infra/http/controllers/CategoryController.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express'
+
 import { CreateCategoriesUseCase } from '../../../useCases/CreateCategoriesUseCase'
 import { ListCategoriesUseCase } from '../../../useCases/ListCategoriesUseCase'
+import { ListProductsByCategory } from '../../../useCases/ListProductsByCategory'
 import { createCategory } from '../../../validations/createCategory'
+import { listProducts } from '../../../validations/listProducts'
 
 export class CategoryController {
   public async index (request: Request, response: Response): Promise<Response> {
@@ -18,5 +21,12 @@ export class CategoryController {
       icon
     })
     return response.status(201).json(category)
+  }
+
+  public async show (request: Request, response: Response): Promise<Response> {
+    const { category_id } = listProducts.parse(request.params)
+    const listProductsByCategory = new ListProductsByCategory()
+    const products = await listProductsByCategory.execute({ category_id })
+    return response.status(201).json(products)
   }
 }
--- src/modules/categories/infra/http/routes/categories.routes.ts
+++ src/modules/categories/infra/http/routes/categories.routes.ts
@@ -8,6 +8,8 @@ categoriesRoutes.get('/categories', category.index)
 
 categoriesRoutes.post('/categories', category.store)
 
+categoriesRoutes.get('/categories/:category_id/products', category.show)
+
 export {
   categoriesRoutes
 }
