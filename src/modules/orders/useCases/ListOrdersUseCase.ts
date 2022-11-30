7b2c86c704623bce9e610efed2c3636c1c3c2c15
Gui-dev <adrianon10@gmail.com>
2022-11-30 04:08:26 -0300

    feat: add helpers functions sort and populate

--- src/modules/orders/useCases/ListOrdersUseCase.ts
+++ src/modules/orders/useCases/ListOrdersUseCase.ts
@@ -4,6 +4,8 @@ import { IOrder, Order } from '../infra/mongo/models/Order'
 export class ListOrdersUseCase {
   public async execute (): Promise<IOrder[]> {
     const orders = await Order.find()
+      .sort({ createdAt: 1 })
+      .populate('products.product')
     if (orders.length <= 0) {
       throw new AppError('Orders not found', 404)
     }
